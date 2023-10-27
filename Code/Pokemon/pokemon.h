946b903f7585fc8b45e7cf3d03b2bcd754bcf65d
Parker <87654583+Like1Shot@users.noreply.github.com>
2023-10-27 14:33:23 -0600

    Update pokemon.h

--- Code/Pokemon/pokemon.h
+++ Code/Pokemon/pokemon.h
@@ -4,7 +4,12 @@ Header files are used to declare a class (sometimes more than one class too) alo
 We only define prototypes inside of a class here in it's public section
 */
 
-// Header guards
+/* ===== Header guards ===== 
+#ifndef MACRO_NAME
+// Code to include if MACRO_NAME is not defined
+#endif
+
+*/
 #ifndef POKEMON_H
 #define POKEMON_H
 
@@ -19,16 +24,15 @@ class Pokemon {
     // Private section of the class
     // These are not directly accessbile by objects of this class - and they are hidden from the outside world
     private:
-        int id_; // the convention is to put an underscore before or after the name of a variable (to indicate that they are held private)
-        string name_;
-        int hp_;
-        int attack_;
-        int defense_;
-        int speed_;
-        string type_;
+        int _id; // the convention is to put an underscore before the name of a variable (to indicate that they are held private)
+        string _name;
+        int _hp;
+        int _attack;
+        int _defense;
+        int _speed;
         
     public:
-        // ----------------- Section for constructors for the class ----------------------------------
+        /* ========== Constructors for the class ========== */
 
         // This is a prototype for a default constructor
         // Note that there is no return type (not even void!) and the name of the function is same as that of the class
@@ -38,7 +42,7 @@ class Pokemon {
         Pokemon(string name, int id);
 
 
-        // ----------------- Section for getters and setters for the class ----------------------------
+        /* =========== Setters and Getters ========== */
 
         // Prototypes for getters (accessors) of the class
         // Try to think why the return types are what they are
@@ -48,7 +52,6 @@ class Pokemon {
         int getAttack() const;
         int getDefense() const;
         int getSpeed() const;
-        string getType() const;
 
         // Prototypes for setters (mutators) of the class
         // Try to think why the return types are void and they take an input parameter here
@@ -58,7 +61,6 @@ class Pokemon {
         void setAttack(int attack);
         void setDefense(int defense);
         void setSpeed(int speed);
-        void setType(string type);
 };
 
 #endif
